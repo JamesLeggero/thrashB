e3a67905f50361d004528973e25604d95603db85
JamesLeggero <james.m.leggero@gmail.com>
2021-04-14 17:57:45 -0400

    scroll function kinda sorta working, not smooth, pure js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-$(()=>{
-    console.log('jQ up')
+// $(()=>{
+//     console.log('jQ up')
+
+//     const scrollTo = (section) => {
+        
+//     }
+
+//     $('.scroll-manifesto').on('click', ()=>{
+//         scrollTo('.music' )
+//     })
+
+
+// })
+
+const goToSection = (event, section) => {
+    event.preventDefault()
+    // console.log(section)
+    const area = document.getElementById(section)
+    area.scrollIntoView()
+}
+
+document.getElementById('scroll-manifesto').addEventListener('click', event => {
+    // event.preventDefault()
+    // console.log('mnifesto')
+    goToSection(event, 'manifesto')
+})
+
+document.getElementById('scroll-news').addEventListener('click', event => {
+    // event.preventDefault()
+    // console.log('mnifesto')
+    goToSection(event, 'news')
 })
