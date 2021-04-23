a3fc1662c7e6ad2ed60a1896c0258471e2f7b53c
JamesLeggero <james.m.leggero@gmail.com>
2021-04-23 11:17:49 -0400

    links added, js taken and replaced with a tags and ids

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 
-const goToSection = (event, section) => {
-    event.preventDefault()
-    // console.log(section)
-    const area = document.getElementById(section)
-    area.scrollIntoView({behavior: 'smooth'})
-}
+// const goToSection = (event, section) => {
+//     event.preventDefault()
+//     // console.log(section)
+//     const area = document.getElementById(section)
+//     area.scrollIntoView({behavior: 'smooth'})
+// }
 
-document.getElementById('scroll-thrash').addEventListener('click', event => {
-    event.preventDefault()
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-})
+// document.getElementById('scroll-thrash').addEventListener('click', event => {
+//     event.preventDefault()
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// })
 
-document.getElementById('scroll-manifesto').addEventListener('click', event => {
-    goToSection(event, 'manifesto')
-})
+// document.getElementById('scroll-manifesto').addEventListener('click', event => {
+//     goToSection(event, 'manifesto')
+// })
 
-document.getElementById('scroll-news').addEventListener('click', event => {
-    goToSection(event, 'news')
-})
+// document.getElementById('scroll-news').addEventListener('click', event => {
+//     goToSection(event, 'news')
+// })
 
-document.getElementById('scroll-music').addEventListener('click', event => {
-    goToSection(event, 'music')
-})
+// document.getElementById('scroll-music').addEventListener('click', event => {
+//     goToSection(event, 'music')
+// })
 
-document.getElementById('scroll-media').addEventListener('click', event => {
-    goToSection(event, 'media')
-})
+// document.getElementById('scroll-media').addEventListener('click', event => {
+//     goToSection(event, 'media')
+// })
 
-document.getElementById('scroll-links').addEventListener('click', event => {
-    goToSection(event, 'links')
-})
+// document.getElementById('scroll-links').addEventListener('click', event => {
+//     goToSection(event, 'links')
+// })
